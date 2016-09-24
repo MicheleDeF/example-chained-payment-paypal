4c832793eb1309b694e02e4409f4a9dfcebf0f9a
michele de falco <defalco_michele@libero.it>
2016-09-24 13:37:00 +0200

    Update lib.php

--- lib.php
+++ lib.php
@@ -10,40 +10,40 @@ protected $signature = "< signature >";
 protected $idapp = "APP-80W284485P519543T";	//ID APP Default Sandbox 
 
 protected function __construct(){
-	
-	$this->paypalUrl = $paypalUrl;
-	$this->apiUrl = $apiUrl;
-	$this->user = $user;
-	$this->password = $password;
-	$this->signature = $signature;
-	$this->idapp = $idapp;
-	
-	
-	
+
+$this->paypalUrl = $paypalUrl;
+$this->apiUrl = $apiUrl;
+$this->user = $user;
+$this->password = $password;
+$this->signature = $signature;
+$this->idapp = $idapp;
+
+
+
 }
 
 protected function get_paypalUrl(){
-	return $this->paypalUrl;
+return $this->paypalUrl;
 }
 
 protected function get_apiUrl(){
-	return $this->apiUrl;
+return $this->apiUrl;
 }
 
 protected function get_user(){
-	return $this->user;
+return $this->user;
 }
 
 protected function get_password(){
-	return $this->password;
+return $this->password;
 }
 
 protected function get_signature(){
-	return $this->signature;
+return $this->signature;
 }
 
 protected function get_idapp(){
-	return $this->idapp;
+return $this->idapp;
 }
 
 }
@@ -51,24 +51,24 @@ protected function get_idapp(){
 class Paypal extends Credential{	
 
 function __construct(){
-	$this->headers = array(
-	"Content-Type: text/html; charset=utf-8",
-	"X-PAYPAL-SECURITY-USERID: " . $this->get_user(),
-	"X-PAYPAL-SECURITY-PASSWORD: " . $this->get_password(),
-	"X-PAYPAL-SECURITY-SIGNATURE: " . $this->get_signature() ,
-	"X-PAYPAL-REQUEST-DATA-FORMAT: JSON" ,
-	"X-PAYPAL-RESPONSE-DATA-FORMAT: JSON" ,
-	"X-PAYPAL-APPLICATION-ID: " . $this->get_idapp()	
-	);
-	
-	$this->apiUrl = $this->get_apiUrl();
-	$this->paypalUrl = $this->get_paypalUrl();
-	
+$this->headers = array(
+"Content-Type: text/html; charset=utf-8",
+"X-PAYPAL-SECURITY-USERID: " . $this->get_user(),
+"X-PAYPAL-SECURITY-PASSWORD: " . $this->get_password(),
+"X-PAYPAL-SECURITY-SIGNATURE: " . $this->get_signature() ,
+"X-PAYPAL-REQUEST-DATA-FORMAT: JSON" ,
+"X-PAYPAL-RESPONSE-DATA-FORMAT: JSON" ,
+"X-PAYPAL-APPLICATION-ID: " . $this->get_idapp()	
+);
+
+$this->apiUrl = $this->get_apiUrl();
+$this->paypalUrl = $this->get_paypalUrl();
+
 
 }
 
 function _paypalSend($data,$call){
-	
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $this->apiUrl.$call);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -83,7 +83,7 @@ return json_decode(curl_exec($ch),TRUE);
 
 
 function splitPay(){
-	
+
 // pay request
 $createPacketPay = array(
 
@@ -98,31 +98,31 @@ $createPacketPay = array(
 
 ),
 "receiverList"=> array(
-					"receiver"=>array(
-									array(
-									"amount"=>"0.10", // example 0.10 euro
-									"email" => "< email_primary_receverer >",
-									"primary"=>false,
-									"paymentType"=>"SERVICE",
-									
-									),
-									array(
-									"amount"=>"2.00", // example 2.00 euro
-									"email"=>"< email_secondary_receverer >", 
-									"primary"=>true,
-									"paymentType"=>"SERVICE"
-																		
-									)
-									)
-						),
+"receiver"=>array(
+array(
+"amount"=>"0.10", // example 0.10 euro
+"email" => "< email_primary_receverer >",
+"primary"=>false,
+"paymentType"=>"SERVICE",
+
+),
+array(
+"amount"=>"2.00", // example 2.00 euro
+"email"=>"< email_secondary_receverer >", 
+"primary"=>true,
+"paymentType"=>"SERVICE"
+
+)
+)
+),
 
 "returnUrl"=>"http://localhost/returnUrl.php",	   //return url
 "cancelUrl"=>"http://localhost/cancelUrl.php",	  //return url in case of cancellation
 "requestEnvelope"=>array(
-                          "errorLanguage"=>"en_US",
-                           "detailLevel"=>"ReturnAll"						  
-                         
-                        ),
+"errorLanguage"=>"en_US",
+"detailLevel"=>"ReturnAll"						  
+
+),
 "reverseAllParallelPaymentsOnError"=>true
 );
 
@@ -132,24 +132,24 @@ return $responsePay;
 
 
 function getPayKey(){
-	
+
 $res = $this->splitPay();
 $payKey = $res['payKey'];
 return $payKey;
-	
+
 }
 
 
 function getJSONresponse(){
-	
+
 $res = $this->splitPay();
 $jsonresponse =  json_encode($res);
 return $jsonresponse;
-	
+
 }
 
 function getPaymentDetails($paykey){
-	
+
 $createPaymentDetails = array(
 
 "payKey"=>$paykey,
@@ -160,7 +160,7 @@ $createPaymentDetails = array(
 //It returns the information of the payment request
 $responsePaymentDetails = $this->_paypalSend($createPaymentDetails,"PaymentDetails");
 return json_encode($responsePaymentDetails);	
-	
+
 }
 
 }
