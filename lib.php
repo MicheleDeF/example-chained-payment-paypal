011ac61c42be2e280902995e01ec628c28129674
michele de falco <defalco_michele@libero.it>
2021-10-17 14:17:54 +0200

    modify 17/10/21

--- lib.php
+++ lib.php
@@ -7,7 +7,7 @@ protected $apiUrl = "https://svcs.sandbox.paypal.com/AdaptivePayments/";
 protected $user = < user_id >;
 protected $password = "< user_password >";
 protected $signature = "< signature >";
-protected $idapp = "APP-80W284485P519543T";	// app sandbox paypal
+protected $idapp = "APP-80W284485P519543T";	// app sandbox paypal for test
 
 protected function __construct(){
 
